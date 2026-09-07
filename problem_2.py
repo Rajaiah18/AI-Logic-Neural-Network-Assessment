ae2edb4317b466356d6976a541269997a0c626a2
Talluru Rajaiah <rajaiahthalluru@gmail.com>
2026-09-07 13:39:12 +0530

    Add solutions for problem 2 and problem 3

---
+++ problem_2.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+k = int(input())
+
+window_sum = sum(arr[:k])
+max_sum = window_sum
+
+for i in range(k, n):
+    window_sum += arr[i]
+    window_sum -= arr[i - k]
+    max_sum = max(max_sum, window_sum)
+
+print(max_sum)
